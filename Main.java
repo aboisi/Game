b32f8861d6406c6d21f3570b1a5d3d33e92ffda9
aboisi <149761398+aboisi@users.noreply.github.com>
2024-04-03 18:25:37 +0900

    勇者とお化けキノコ2匹を生み出す

--- Main.java
+++ Main.java
@@ -5,11 +5,25 @@ public class Main {
         //2.フィールドに初期値をセット
         h.name = "ミナト";
         h.hp = 100;
-        System.out.println("勇者" + h.name + "を生み出しました！");
-        //3.勇者のメソッドを呼び出していく
-        h.sit(5);
+
+        Matango m1 = new Matango();//お化けキノコAを生成し、初期化
+        m1.hp = 50;
+        m1.suffix = 'A';
+
+        Matango m2 = new Matango();//お化けキノコBを生成し、初期化
+        m2.hp = 48;
+        m2.suffix = 'B';
+
+        //冒険の始まり
         h.slip();
-        h.sit(25);
+        m1.run();
+        m2.run();
         h.run();
+        // System.out.println("勇者" + h.name + "を生み出しました！");
+        // //3.勇者のメソッドを呼び出していく
+        // h.sit(5);
+        // h.slip();
+        // h.sit(25);
+        // h.run();
     }
 }
