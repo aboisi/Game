16b606a77f1adc0ec09fb8361eee6695138e3cf5
aboisi <149761398+aboisi@users.noreply.github.com>
2024-04-03 15:31:39 +0900

    Heroクラスにsleepメソッド宣言

--- Hero.java
+++ Hero.java
@@ -4,7 +4,10 @@ public class Hero {
     int hp;
     //操作の定義
     public void attack() {}
-    public void sleep() {}
+    public void sleep() {
+        this.hp = 100;//自分自身のインスタンスのhpフィールドに100を代入する
+        System.out.println(this.name + "は、眠って回復した！");
+    }
     public void sit(int sec) {}
     public void slip() {}
     public void run() {}
