96a1bab675198502f6ae590e740a66c473cccb4f
aboisi <149761398+aboisi@users.noreply.github.com>
2024-04-03 16:09:41 +0900

    Heroクラスからインスタンスを生成

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        //勇者よ、この仮想世界に生まれよ！
+        //1.勇者を生成
+        Hero h = new Hero();//Heroクラスからインスタンスを生成し、変数hに入れる
         //お化けキノコよ、この仮想世界に生まれよ！
         //勇者よ、戦え！
         //お化けキノコよ、逃げろ！
